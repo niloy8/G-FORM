e6024e1d046058bb7cd683d52b540b8e9ebc592e
niloy8 <niloybhuiyan16810@gmail.com>
2024-07-30 11:53:26 +0600

    daynamic-title

--- js/untitled.js
+++ js/untitled.js
@@ -4,14 +4,20 @@ const form = document.querySelector('.form input')
 const title = "Untitled Form"
 form.value = title
 const formInput = document.querySelector('.form-description input')
-
+console.log(formInput);
 formInput.value = form.value
+
 const handleFoucsChange = e => {
     if (e.type === 'blur') {
-        formInput.value = form.value
+        formInput.value = e.target.value
     }
-
 }
 
-form.addEventListener('blur', handleFoucsChange)
-formInput.addEventListener('blur', handleFoucsChange)
+const handleFoucsChange1 = e => {
+    if (e.target.value === '') {
+        formInput.value = form.value
+    }
+}
+
+form.addEventListener("blur", handleFoucsChange)
+formInput.addEventListener("keyup", handleFoucsChange1)
