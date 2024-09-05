f92d5d8ed6267fbabec4c3865781a5835cb7eaba
niloy8 <niloybhuiyan16810@gmail.com>
2024-09-05 13:16:29 +0600

    text-formator added

--- js/dynamicContainer.js
+++ js/dynamicContainer.js
@@ -1,15 +1,23 @@
 const Input = document.querySelectorAll('.form-description input')
-const daynamicContainer = document.querySelectorAll('.dynamic-container')
-const p = document.createElement('p')
-p.textContent = "hello world"
-p.classList.add('daynamic-div')
+const descriptionFormatting = document.getElementById('description-formatting')
+const formatingDiv = document.createElement('div')
+formatingDiv.innerHTML = `
+
+    <i class="fa-sharp fa-solid fa-bold"></i>
+    <i class="fa-solid fa-italic"></i>
+    <i class="fa-solid fa-underline"></i>
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"     stroke="#5f6368" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link-2"><path d="M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3"></path><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+    <i class="fa-solid fa-text-slash"></i>
+
+`
+formatingDiv.classList.add('daynamic-textformat-div')
 let isFocused = false
 
 const handleDaynamicDiv = (e) => {
     if (!isFocused) {
         const target = e.target
-        target.parentNode.insertBefore(p, target.nextSibling)
-        p.style.display = ''
+        target.parentNode.insertBefore(formatingDiv, target.nextSibling)
+        formatingDiv.style.display = ''
         isFocused = true
     }
 }
@@ -17,8 +25,7 @@ const handleDaynamicDiv = (e) => {
 const handleBlur = (ev) => {
     if (isFocused) {
         const pClassname = ev.target.nextSibling
-
-        p.style.display = 'none'
+        formatingDiv.style.display = 'none'
         isFocused = false
     }
 }
