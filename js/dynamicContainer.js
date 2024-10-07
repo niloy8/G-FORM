59e9d51e00b532996fa072f246d977ac920ee00b
niloy8 <niloybhuiyan16810@gmail.com>
2024-10-08 00:11:44 +0600

    Bug fix

--- js/dynamicContainer.js
+++ js/dynamicContainer.js
@@ -5,12 +5,7 @@ formatingDiv.innerHTML = `
     <i data-tooltip="Bold"  class="fa-sharp fa-solid fa-bold"></i>
     <i data-tooltip="Italic" class="fa-solid fa-italic"></i>
     <i data-tooltip="Underline" class="fa-solid fa-underline"></i>
-    <div data-tooltip="Insert Link" class="svg-container">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#5f6368" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link-2">
-            <path d="M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3"></path>
-            <line x1="8" y1="12" x2="16" y2="12"></line>
-        </svg>
-    </div>
+    <ion-icon data-tooltip="Insert Link" name="link-outline"></ion-icon>
     <i data-tooltip="Remove Formatting" class="fa-solid fa-text-slash"></i>
 `
 
